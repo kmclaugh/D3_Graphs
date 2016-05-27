8d5646e768cc381e88cfef9302c90fc6d07bafaa
Kevin McLaughlin <kevin.mclaughlin70@gmail.com>
2016-05-27 18:03:46 -0500

    Working on presidential qualifications

--- presidential_qualifications/convert_to_json.py
+++ presidential_qualifications/convert_to_json.py
@@ -9,26 +9,28 @@ candidate_dictionary = {}
 for row in dataframe.iterrows():
     president = row[1][0]
     if president not in candidate_dictionary:
-        position_list = [{'Position': row[1][1], 'Start_Date':row[1][2], 'End_Date':row[1][3], 'Years_of_Experience':row[1][4]}]
-        candidate_dictionary[president] = {'experience': position_list}
+        candidate_dictionary[president] = {'experience': [], 'administration_start': row[1][2], 'administration_end':row[1][3], 'administration_length': row[1][4]}
         
-    ##Candidate Position	Start Date	End Date	Years of Experience	Points
-    candidate_dictionary[president]['experience'].append({'Position': row[1][1], 'Start_Date':row[1][2], 'End_Date':row[1][3], 'Years_of_Experience':row[1][4]})
+    else:
+        candidate_dictionary[president]['experience'].append({'Position': row[1][1], 'Start_Date':row[1][2], 'End_Date':row[1][3], 'Years_of_Experience':row[1][4]})
 
 candidate_list = []
 for candidate, value in candidate_dictionary.iteritems():
-    new_object = {'name': candidate, 'experience': value['experience']}
+    new_object = {
+                    'name': candidate, 'experience': value['experience'],
+                    'administration_start': value['administration_start'], 'administration_end':value['administration_end'],
+                    'administration_length': value['administration_length']
+                  }
     candidate_list.append(new_object)
 
 with open('Candidates.json', 'w') as fp:
     json.dump(candidate_list, fp)
-#print(dataframe.dtypes)
-#dataframe.to_json('data.json', orient="records")
-#csvfile = open('worst_atrocities.csv', 'r')
-#jsonfile = open('worst_atrocities.json', 'w')
-#
-#fieldnames = ("Cause","Year","Death_Total","Relative","Century_String")
-#reader = csv.DictReader(csvfile, fieldnames, quoting=csv.QUOTE_NONNUMERIC)
-#for row in reader:
-#    json.dump(row, jsonfile)
-#    jsonfile.write('\n')
+    
+dataframe = pandas.read_csv('Points_per_Position.csv')
+
+Points_per_Position = {}
+for row in dataframe.iterrows():
+    Points_per_Position[row[1][0]] = row[1][1]
+print(Points_per_Position)
+with open('Points_per_Position.json', 'w') as fp:
+    json.dump(Points_per_Position, fp)
--- presidential_qualifications/presidential_qualifications.js
+++ presidential_qualifications/presidential_qualifications.js
@@ -1,19 +1,72 @@
 $(window).load(function () {
     
     $(document).ready(function () {
-        data = jQuery.parseJSON( data )
-        for (president in data){
-            //console.log(data[president])
-            for (position in data[president]){
-                //console.log(president, position)
-                for (experience in data[president][position]){
-                    var value = data[president][position][experience];
-                    if (typeof value == "string" && value.indexOf('/') > -1){
-                        data[president][position][experience] = moment(value, "MM/DD/YYYY")
-                    }
-                }
-            }
+        
+        candidate_list = [];
+        for (i=0; i<candidates_data.length; i++){
+            candidate_data = candidates_data[i];
+            candidate = new candidate_class(name=candidate_data.name, experience=candidate_data.experience, administration_start=candidate_data.administration_start, administration_end=candidate_data.administration_end, administration_length=candidate_data.administration_length)
+            candidate.init_data();
+            candidate_list.push(candidate)
         }
-        console.log(data)
+        console.log(Points_per_Position);
+        console.log(candidate_list);
     });
 });
+
+
+function candidate_class(name, experience, administration_start, administration_end, administration_length ){
+    /*Class for carrying around candidate info*/
+    
+    var self = this;
+    self.name = name;
+    self.experience = experience;
+    self.administration_start = moment(administration_start, "MM/DD/YYYY");
+    self.administration_end = moment(administration_end, "MM/DD/YYYY");;
+    self.administration_length = administration_length;
+    
+    self.init_data = function(){
+        self.experience.forEach(function(position){
+            if (position['Start_Date'] != null){
+                position['Start_Date'] = moment(position['Start_Date'], "MM/DD/YYYY");
+            }
+            if (position['End_Date'] != null){
+                position['End_Date'] = moment(position['End_Date'], "MM/DD/YYYY");
+            }
+            position['experience_percentage'] = experience_curve(position['Years_of_Experience']);
+        });
+    }
+    
+    self.calculate_experience_points = function(){
+        /*Calculates the total experience and stores it as the attribute 'experience_points'*/
+        var experience_points = 0;
+        self.experience.forEach(function(position){
+            experience_points += Points_per_Position[position['Position']] * position['experience_percentage'];
+        });
+}
+
+experience_curve = function(t){
+    /*Returns the the percent of total experience using the function Generalized Logistic Function used to calulcate
+     *given a time t spent in the position
+     *link: https://en.wikipedia.org/wiki/Generalised_logistic_function
+     *https://docs.google.com/spreadsheets/d/1BaL6V2jQZvppoQoKpLySdvj2q8v0Kq7J_ccpoejdvyE/edit#gid=1026319676
+     * Y = A+(K-A)/POW(C+Q*EXP(-B*(t-M)), 1/v)
+     */
+    var A = 0.034,
+        K = 1,
+        B = 0.7,
+        Q = 0.5,
+        M = 3.7,
+        C = 1,
+        v = 0.5;
+    if (t > 0){
+        var experience_percent = A+(K-A)/Math.pow(C+Q*Math.exp(-B*(t-M)), 1/v);
+        experience_percent = Math.round(experience_percent*100)/100; // Round to two decimal points
+    }
+    else{
+        var experience_percent = 0;
+    }
+    
+    return experience_percent;
+    
+}
