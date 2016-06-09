d6e5c1904b0418d7e1409c6afe96941541e858c8
Kevin McLaughlin <kevin.mclaughlin70@gmail.com>
2016-06-09 07:23:23 -0500

    Saving d3 table

--- presidential_qualifications/points_table.js
+++ presidential_qualifications/points_table.js
@@ -3,9 +3,9 @@ function points_table_class(the_data, graph_container_id, title_text, slug){
     /*Class for the points per positions table*/
     
     var self = this;
-    self.data = the_data;
+    self.data = the_data
     self.graph_container_id = graph_container_id;
-    self.columns = ["Position", "Points"]
+    self.columns = ["Position", "Deafult Points", "Your Points"]
     console.log(self.data)
     self.update_data = function(group){
         
@@ -29,6 +29,30 @@ function points_table_class(the_data, graph_container_id, title_text, slug){
             .enter()
             .append("th")
                 .text(function(column) { return column; });
+        
+        // create a row for each object in the data
+        self.rows = self.tbody.selectAll("tr")
+            .data(self.data)
+            .enter()
+            .append("tr");
+        
+        // create a cell in each row for each column
+        self.cells = self.rows.selectAll("td")
+            .data(function(row) {
+                console.log(row)
+                return self.columns.map(function(column) {
+                    if (column != "Your Points"){
+                        return {column: column, value: row[column]};
+                    }
+                    else{
+                        return {column: column, value: '<input name="position_points" position="'+row.Position+' " value="'+row["Default Points"]+'>1</input>'};
+                    }
+                });
+            })
+            .enter()
+            .append("td")
+            .attr("style", "font-family: Courier") // sets the font style
+                .html(function(d) { return d.value; });
         
     }//End draw graph
     
