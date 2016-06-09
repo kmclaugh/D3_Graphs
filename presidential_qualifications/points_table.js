f6f01cad56aae70e7cf03e431ea955a978ae6351
Kevin McLaughlin <kevin.mclaughlin70@gmail.com>
2016-06-09 07:23:54 -0500

    Saving jquery table

--- presidential_qualifications/points_table.js
+++ presidential_qualifications/points_table.js
@@ -4,8 +4,9 @@ function points_table_class(the_data, graph_container_id, title_text, slug){
     
     var self = this;
     self.data = the_data
+    self.columns = ["Position", "Deafult Points", "Your Points"];
     self.graph_container_id = graph_container_id;
-    self.columns = ["Position", "Deafult Points", "Your Points"]
+    self.graph_element = $('#'+self.graph_container_id);
     console.log(self.data)
     self.update_data = function(group){
         
@@ -17,42 +18,26 @@ function points_table_class(the_data, graph_container_id, title_text, slug){
 
     self.create = function(){
         /*creates the table*/
-        self.table = d3.select('#'+self.graph_container_id)
-            .append("table")
+       
+        self.graph_element.append('<table id="table_'+self.graph_container_id+'" ></table>');
+        self.table = $('#table_'+self.graph_container_id);
         
-        self.thead = self.table.append("thead");
-        self.tbody = self.table.append("tbody");
-
-        self.thead.append("tr")
-            .selectAll("th")
-            .data(self.columns)
-            .enter()
-            .append("th")
-                .text(function(column) { return column; });
+        self.table.append('<thead></thead>');
+        self.thead = $('#table_'+self.graph_container_id+'> thead');
+        self.columns.forEach(function(column){
+            self.thead.append('<th>'+column+'</th>')
+        });
         
-        // create a row for each object in the data
-        self.rows = self.tbody.selectAll("tr")
-            .data(self.data)
-            .enter()
-            .append("tr");
-        
-        // create a cell in each row for each column
-        self.cells = self.rows.selectAll("td")
-            .data(function(row) {
-                console.log(row)
-                return self.columns.map(function(column) {
-                    if (column != "Your Points"){
-                        return {column: column, value: row[column]};
-                    }
-                    else{
-                        return {column: column, value: '<input name="position_points" position="'+row.Position+' " value="'+row["Default Points"]+'>1</input>'};
-                    }
-                });
-            })
-            .enter()
-            .append("td")
-            .attr("style", "font-family: Courier") // sets the font style
-                .html(function(d) { return d.value; });
+        self.table.append('<tbody></thead>');
+        self.tbody = $('#table_'+self.graph_container_id+'> tbody');
+       
+        for (key in self.data){
+            var default_points = self.data[key];
+            var position_cell = '<td name="Position" data="'+key+'" >'+key+'</td>';
+            var deafult_points_cell = '<td name="Default Points" data="'+default_points+'" >'+default_points+'</td>';
+            var your_points_cell = '<td name="Your Points" data="'+default_points+'" >'+default_points+'</td>';
+             console.log(key, self.data[key])
+        }
         
     }//End draw graph
     
