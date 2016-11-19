caea6b5560f1da79c357ecbf26407d1d4f02cf5e
Kevin McLaughlin <kevin.mclaughlin70@gmail.com>
2016-11-19 09:48:03 -0600

    Updated scores

--- smart_hiring/data.js
+++ smart_hiring/data.js
@@ -1,16 +1,16 @@
 var candidateData = [
   {
     "Name": "George",
-    "Normalized Interview Score": 109.1666667,
-    "Normalized Resume Score": 71.95121951,
-    "Final Score": 191.9011882,
-    "Resume Score": 632.5,
+    "Normalized Interview Score": 67.56410256,
+    "Normalized Resume Score": 68.90243902,
+    "Final Score": 170.9177611,
+    "Resume Score": 565,
     "Interview Score": 87.83333333,
     "Predicted Final Score": null
   },
   {
     "Name": "Winston",
-    "Normalized Interview Score": 81.16666667,
+    "Normalized Interview Score": 62.43589744,
     "Normalized Resume Score": 67.37804878,
     "Final Score": 163.5029706,
     "Resume Score": 552.5,
@@ -19,7 +19,7 @@ var candidateData = [
   },
   {
     "Name": "Chrsitine",
-    "Normalized Interview Score": 78.16666667,
+    "Normalized Interview Score": 60.12820513,
     "Normalized Resume Score": 80.18292683,
     "Final Score": 180.4025954,
     "Resume Score": 657.5,
@@ -28,7 +28,7 @@ var candidateData = [
   },
   {
     "Name": "Caroline",
-    "Normalized Interview Score": 72,
+    "Normalized Interview Score": 55.38461538,
     "Normalized Resume Score": 72.86585366,
     "Final Score": 164.6833959,
     "Resume Score": 597.5,
@@ -37,43 +37,43 @@ var candidateData = [
   },
   {
     "Name": "Sigmond",
-    "Normalized Interview Score": 62.66666667,
-    "Normalized Resume Score": 60.67073171,
-    "Final Score": 139.2112258,
+    "Normalized Interview Score": 52.30769231,
+    "Normalized Resume Score": 60.06097561,
+    "Final Score": 142.3991557,
     "Resume Score": 492.5,
     "Interview Score": 68,
     "Predicted Final Score": null
   },
   {
     "Name": "James",
-    "Normalized Interview Score": 59.33333333,
-    "Normalized Resume Score": 55.18292683,
-    "Final Score": 128.4154159,
+    "Normalized Interview Score": 48.20512821,
+    "Normalized Resume Score": 60.67073171,
+    "Final Score": 139.2112258,
     "Resume Score": 497.5,
     "Interview Score": 62.66666667,
     "Predicted Final Score": null
   },
   {
     "Name": "Brad",
-    "Normalized Interview Score": 57.83333333,
-    "Normalized Resume Score": 48.7804878,
-    "Final Score": 117.6579112,
+    "Normalized Interview Score": 45.64102564,
+    "Normalized Resume Score": 55.18292683,
+    "Final Score": 128.4154159,
     "Resume Score": 452.5,
     "Interview Score": 59.33333333,
     "Predicted Final Score": null
   },
   {
     "Name": "Mary",
-    "Normalized Interview Score": 46.66666667,
-    "Normalized Resume Score": 60.06097561,
-    "Final Score": 125.9888993,
+    "Normalized Interview Score": 44.48717949,
+    "Normalized Resume Score": 48.7804878,
+    "Final Score": 117.6579112,
     "Resume Score": 400,
     "Interview Score": 57.83333333,
     "Predicted Final Score": null
   },
   {
     "Name": "Stacy",
-    "Normalized Interview Score": 41.16666667,
+    "Normalized Interview Score": 31.66666667,
     "Normalized Resume Score": 57.01219512,
     "Final Score": 117.1849593,
     "Resume Score": 467.5,
@@ -82,7 +82,7 @@ var candidateData = [
   },
   {
     "Name": "Randal",
-    "Normalized Interview Score": 31.5,
+    "Normalized Interview Score": 24.23076923,
     "Normalized Resume Score": 62.19512195,
     "Final Score": 117.5234522,
     "Resume Score": 510,
@@ -96,7 +96,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 555,
     "Interview Score": 0,
-    "Predicted Final Score": 153.7508518
+    "Predicted Final Score": 154.1896274
   },
   {
     "Name": "Vickey",
@@ -105,7 +105,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 625,
     "Interview Score": 0,
-    "Predicted Final Score": 173.8486036
+    "Predicted Final Score": 174.2789455
   },
   {
     "Name": "Simone",
@@ -114,7 +114,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 632.5,
     "Interview Score": 0,
-    "Predicted Final Score": 176.0019341
+    "Predicted Final Score": 176.4313724
   },
   {
     "Name": "Sandra",
@@ -123,7 +123,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 510,
     "Interview Score": 0,
-    "Predicted Final Score": 140.8308686
+    "Predicted Final Score": 141.2750658
   },
   {
     "Name": "Richard",
@@ -132,7 +132,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 422.5,
     "Interview Score": 0,
-    "Predicted Final Score": 115.7086789
+    "Predicted Final Score": 116.1634183
   },
   {
     "Name": "Rachael",
@@ -141,7 +141,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 502.5,
     "Interview Score": 0,
-    "Predicted Final Score": 138.677538
+    "Predicted Final Score": 139.1226389
   },
   {
     "Name": "Porter",
@@ -150,7 +150,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 485,
     "Interview Score": 0,
-    "Predicted Final Score": 133.6531001
+    "Predicted Final Score": 134.1003094
   },
   {
     "Name": "Nate",
@@ -159,7 +159,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 480,
     "Interview Score": 0,
-    "Predicted Final Score": 132.2175464
+    "Predicted Final Score": 132.6653581
   },
   {
     "Name": "Matt",
@@ -168,7 +168,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 100,
     "Interview Score": 0,
-    "Predicted Final Score": 23.11546557
+    "Predicted Final Score": 23.60906027
   },
   {
     "Name": "Lincoln",
@@ -177,7 +177,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 332.5,
     "Interview Score": 0,
-    "Predicted Final Score": 89.8687124
+    "Predicted Final Score": 90.33429514
   },
   {
     "Name": "Kathryn",
@@ -186,7 +186,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 465,
     "Interview Score": 0,
-    "Predicted Final Score": 127.9108853
+    "Predicted Final Score": 128.3605043
   },
   {
     "Name": "Karrin",
@@ -195,7 +195,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 527.5,
     "Interview Score": 0,
-    "Predicted Final Score": 145.8553065
+    "Predicted Final Score": 146.2973954
   },
   {
     "Name": "John",
@@ -204,16 +204,16 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 565,
     "Interview Score": 0,
-    "Predicted Final Score": 156.6219592
+    "Predicted Final Score": 157.05953
   },
   {
     "Name": "Jimmy",
     "Normalized Interview Score": 0,
     "Normalized Resume Score": 54.87804878,
     "Final Score": null,
-    "Resume Score": 475,
+    "Resume Score": 450,
     "Interview Score": 0,
-    "Predicted Final Score": 130.7819927
+    "Predicted Final Score": 124.0556504
   },
   {
     "Name": "Jerrie",
@@ -222,7 +222,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 450,
     "Interview Score": 0,
-    "Predicted Final Score": 123.6042242
+    "Predicted Final Score": 124.0556504
   },
   {
     "Name": "Ivan",
@@ -231,7 +231,7 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 475,
     "Interview Score": 0,
-    "Predicted Final Score": 130.7819927
+    "Predicted Final Score": 131.2304068
   },
   {
     "Name": "Eric",
@@ -240,33 +240,33 @@ var candidateData = [
     "Final Score": null,
     "Resume Score": 487.5,
     "Interview Score": 0,
-    "Predicted Final Score": 134.3708769
+    "Predicted Final Score": 134.8177851
   },
   {
     "Name": "Carl",
     "Normalized Interview Score": 0,
     "Normalized Resume Score": 48.7804878,
     "Final Score": null,
-    "Resume Score": 480,
+    "Resume Score": 400,
     "Interview Score": 0,
-    "Predicted Final Score": 132.2175464
+    "Predicted Final Score": 109.7061375
   },
   {
     "Name": "Arletta",
     "Normalized Interview Score": 0,
     "Normalized Resume Score": 54.26829268,
     "Final Score": null,
-    "Resume Score": 400,
+    "Resume Score": 445,
     "Interview Score": 0,
-    "Predicted Final Score": 109.2486873
+    "Predicted Final Score": 122.6206991
   },
   {
     "Name": "Adeline",
     "Normalized Interview Score": 0,
     "Normalized Resume Score": 59.45121951,
     "Final Score": null,
-    "Resume Score": 445,
+    "Resume Score": 487.5,
     "Interview Score": 0,
-    "Predicted Final Score": 122.1686705
+    "Predicted Final Score": 134.8177851
   }
 ]
